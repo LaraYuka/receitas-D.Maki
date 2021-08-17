cf5c65e7a0438b9040c09fa2d84d96f22479713f
Lara Sakanaka <85855158+LaraYuka@users.noreply.github.com>
2021-08-17 02:59:55 +0000

    metade do projeto

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+$("nav div").click(function(){
+  $("ul").slideToggle();
+  $("ul ul").css("display", "none");
+});
+
+$("ul li").click(function() {
+  $("ul ul").slideUp();
+  $(this).find('ul').slideToggle();
+});
+
+$(window).resize(function(){
+  if($(window).width() > 768){
+  $("ul").removeAttr('style');
+});
+
+})
